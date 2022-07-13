5def70042f6fd54d798acd1a5554aced920fea2a
Andrew Wokabi <drewzote@Andrews-MBP.home>
2022-07-13 01:52:43 -0700

    jaskis mongo db queries

--- commands.js
+++ commands.js
@@ -4,34 +4,65 @@
 // GETTING STARTED
 // 1. Create a database called jaskis
 
+use jaskis
+Made sure it was successful by 'running db'.
 
 // 2. Create a collection called bounties
 
+db.createCollection('bounties')
+Made sure it was successful by running 'show collections'.
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-
+{
+    name: "Thanoceros",
+    species: "Rhinoceros",
+    location: "Grasslands",
+    wantedFor: "Eating too much grass",
+    client: "Songbird",
+    reward: 10000,
+    captured: false
+  }
 
 // 2. Query for all bounties in the bounties collection
 
+show bounties
 
 // 3. Insert many bounties at once using the given objects
 
+db.jaskis.insertMany ([])
+
 // MANAGE THE DATABASE
 // Queries
+
 // 1. Query for all bounties in the Grasslands
+
+show Grasslands
 
 // 2. Query for all bounties with a reward worth 10000 or more
 
+db.jaskis.find({bounties: {$gt: 10000 } })
+
 // 3. Query for all bounties, but exclude the client attribute from being shown
 
+
+
 // 4. Query for a Groundhog in the Woodlands
+
+db.jaskis.woodlands.find(Groundhog)
+
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
 
 // 2. Remove Lokinkajou
 
+db.jaskis.deleteOne({ name: "Lokinkajou"})
+
 // 3. Delete all bounties sent by Songbird
 
+db.jaskis.deleteAll({ "Songbird": bounties})
+
 // 4. Update all captured statuses to true
+
+db.jaskis.updateAll ({})
